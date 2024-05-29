977b375328b58e652ac58820cda750d014ff1cdf
siwakasen <siwakasen@gmail.com>
2024-05-30 01:09:20 +0700

    try google nextauth

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -6,36 +6,9 @@ export default function DashboardLayout({ children }) {
     const { data: session, status } = useSession();
     const [insert, setInsert] = useState(false);
     const Router = useRouter()
-    useEffect(() => {
-        if (status === "loading") return
-        if (!session) {
-            Router.push("/auth")
-        } else {
-            console.log(insert);
-            if (!insert) {
-                const setSessionInBackend = async () => {
-                    await fetch(`${process.env.NEXT_PUBLIC_API_URL}/set-session`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        credentials: 'include',
-                        body: JSON.stringify({ user: session.user })
-                    });
-                };
-                setSessionInBackend();
-                setInsert(true);
-            }
-        }
-    }, [session, status]);
-
-    if (status === "authenticated") {
-        return (
-            <div className="flex flex-col items-center justify-center min-h-screen max-h-full w-full bg-gray-900" >
-                {children}
-            </div >
-        );
-    } else {
-        return null
-    }
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen max-h-full w-full bg-gray-900" >
+            {children}
+        </div >
+    );
 }
--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,48 +2,52 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import axios from "axios";
 import SignToken from "./SignToken";
+
 const authOptions = {
     secret: process.env.NEXT_PUBLIC_JWT_SECRET_KEY,
     providers: [
         GoogleProvider({
             clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
             clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+            scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email ',
         }),
     ],
-
     pages: {
         signIn: '/',
         signOut: '/auth',
         error: '/auth/error',
     },
     callbacks: {
-        async signIn({ profile, account }) {
+        async signIn({ account, profile, email, credentials }) {
             if (account.provider === "google") {
-                const data = {
-                    name: profile.name,
-                    email: profile.email,
-                    image: profile.picture,
-                };
-                const response = await axios.post(
-                    "http://localhost:3001/api/auth/login",
-                    data
-                );
+                try {
+                    const data = {
+                        name: profile.name,
+                        email: profile.email,
+                        image: profile.picture,
+                    };
+                    const response = await axios.post(
+                        "http://localhost:3001/api/auth/login",
+                        data
+                    );
+
+                } catch (error) {
+                    console.error("Error during sign-in:", error);
+                    return false;
+                }
             }
             return true;
         },
-        async jwt({ token }) {
-            const userLoggedIn = SignToken(token?.email);
-            token.loggedUser = userLoggedIn;
+        async jwt({ token, user }) {
+            if (user) {
+                token.loggedUser = SignToken(user?.email);
+            }
             return token;
         },
         async session({ session, token }) {
             session.loggedUser = token.loggedUser;
             return session;
         },
-    },
-    debug: true,
-    session: {
-        jwt: true,
     },
 };
 
